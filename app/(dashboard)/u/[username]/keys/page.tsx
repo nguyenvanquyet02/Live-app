0655453e343adc252c19392cd72367d95868f7e8
nguyenvanquyet02 <quyet2k2ocsa@gmail.com>
2024-05-14 23:15:58 +0700

    feat(screens): key page - fix key page ui

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -16,11 +16,13 @@ const KeysPage = async () => {
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold">Keys & URLs</h1>
-        <ConnectModal />
       </div>
       <div className="space-y-4">
         <UrlCard value={stream.serverUrl} />
         <KeyCard value={stream.streamKey} />
+      </div>
+      <div className="mt-4 w-full flex justify-center">
+        <ConnectModal />
       </div>
     </div>
   );
